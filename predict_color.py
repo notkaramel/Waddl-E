fe17d2e0683560180b54858f0ab90a7e22cfbf20
Antoine <antoine.phan@mail.mcgill.ca>
2023-03-10 17:25:11 -0500

    working on color detection

---
+++ predict_color.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This program uses the color sensor at port S3.
+
+Takes in a RGB value (or a list of sample value)
+to detect the seeing color.
+
+It uses data collected generated from `collect_colors.py`
+and ./data/color_data_*.csv files
+
+Author: Antoine Phan @notkaramel
+"""
+
+from utils.brick import EV3ColorSensor, wait_ready_sensors
+from time import sleep
+
+
+def normalize_data(color_file):
+    
+
+if '__name__' == '__main__':
+    pass
