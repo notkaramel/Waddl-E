02a47c217f8613d80465870e0df22f0fc7975cd5
Antoine <antoine.phan@mail.mcgill.ca>
2023-03-26 12:28:20 -0400

    new integration test

--- Test_Unloading.py
+++ Test_Unloading.py
@@ -17,13 +17,30 @@ def test_rollTray():
     rollTray(dps=dps, delay=delay, position=angle)
     rollTray(dps, delay, position=-angle)
 
+def test_LeverAndTray():
+    print("TRAY: (using blind test results)")
+    tray_dps = 300 # int(input("Tray DPS: ")) 
+    tray_angle = 110
+    retrieve = lambda slot: rollTray(tray_dps, 2, slot*tray_angle)
+    
+    lever_dps = 120
+    lever_angle = 90
+    swing = lambda angle : swingLever(lever_dps, 2, angle)
+    
+    retrieve(1)
+    swing(-lever_angle)
+    swing(lever_angle)
+    retrieve(-1)
+
 def test():
     try:
-        test_mode = int(input("[1] swingLever test:\n[2] rollTray test:\nTest mode: "))
+        test_mode = int(input("[1] swingLever test:\n[2] rollTray test:\n[3] Lever and Tray test\nTest mode: "))
         if test_mode == 1:
             test_swingLever()
         elif test_mode == 2:
             test_rollTray()
+        elif test_mode == 3:
+            test_LeverAndTray()
         else:
             print("Try again")
             test()
