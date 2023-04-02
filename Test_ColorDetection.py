543a53be2ff24db94fb9a5d5fb9cb043fe9c5334
Antoine <antoine.phan@mail.mcgill.ca>
2023-04-02 15:20:10 -0400

    Fixing ColorDetection

--- Test_ColorDetection.py
+++ Test_ColorDetection.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 from time import sleep
-from ColorDetection import detects_RGB, FRONT_SENSOR, AVAILABLE_COLORS
+from ColorDetection import detects_RGB, SIDE_SENSOR, FRONT_SENSOR, AVAILABLE_COLORS
 from utils.brick import wait_ready_sensors
 
-def test(input_RGB:list):
+def test(Sensor,input_RGB:list):
     """
     A single test for color detection.
     Display result in markdown form for README.md
@@ -20,7 +20,10 @@ def test_color_detection():
     print("""| RGB Value | Detected Color |""")
     wait_ready_sensors(True)
     while True:
-        test(FRONT_SENSOR.get_rgb())
-        sleep(0.5)
+        print("Front: ", end="")
+        test(FRONT_SENSOR, FRONT_SENSOR.get_rgb())
+        print("Side: ", end="")
+        test(SIDE_SENSOR, SIDE_SENSOR.get_rgb())
+        sleep(0.75)
 
 test_color_detection()
