6798a6183133a21f0e21a26038727860b12872b9
Antoine <antoine.phan@mail.mcgill.ca>
2023-04-02 21:01:46 -0400

    Update Vehicle to turn around

--- Vehicle.py
+++ Vehicle.py
@@ -102,6 +102,6 @@ def turnAround():
     go(power=30)
     import random
     direction = random.choice(['left', 'right'])
-    turn(direction=direction, delay=10)
+    turn(direction=direction, delay=7)
     
     
